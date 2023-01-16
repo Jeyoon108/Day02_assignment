bee2df392919bb0fbdf8868ec1a301b3058b4a02
Jeyoon <rj1031@naver.com>
2023-01-16 17:00:24 +0900

    Chap6 #2

--- day03_assignment.py
+++ day03_assignment.py
@@ -1,7 +1,14 @@
-# Chap 5 #6
+# Chap 6 #2
 
-str = input("input name : ")
-str = str.capitalize()
+guess_me = int(input("Input any number : ")) #7
+number = int(input("Input lower number : ")) #1
 
-
-print(f'{str}y Mc{str}face')
+while number <= guess_me:
+    if number < guess_me:
+        print(f"It's not {number}, still too low")
+        number += 1
+    else:
+        print(f"found it! Input number was {guess_me}")
+        break
+else:
+    print('oops,,')
