15f9c504d98c71fa28a0c73ebaa0cc907f0d9123
Jeyoon <rj1031@naver.com>
2023-01-16 17:06:36 +0900

    Chap6 #3

--- day03_assignment.py
+++ day03_assignment.py
@@ -1,14 +1,11 @@
-# Chap 6 #2
+# Chap 6 #3
 
-guess_me = int(input("Input any number : ")) #7
-number = int(input("Input lower number : ")) #1
+guess_me = int(input("Input any number : ")) #5
 
-while number <= guess_me:
+for number in range(10):
     if number < guess_me:
         print(f"It's not {number}, still too low")
-        number += 1
     else:
         print(f"found it! Input number was {guess_me}")
         break
-else:
-    print('oops,,')
+
