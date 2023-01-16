6cdc9b7ca8e2be81952be7554b3848ce75b463df
Jeyoon <rj1031@naver.com>
2023-01-16 10:24:12 +0900

    moddified_day03

--- day02_assignment.py
+++ day02_assignment.py
@@ -1,7 +1,24 @@
-# Chap4 연습문제 #2
+# Chap4 연습문제 #1
+# import random
+#
+# secret = random.randint(1, 10)
+# guess = int(input('정답은 ?'))
+#
+# if secret == guess:
+#     print("정답 입니다!")
+# elif guess > secret:
+#     print(f"정답보다 큰 수 입니다. 정답은{secret}")
+# else:
+#     print(f"정답보다 작은 수 입니다. 정답은{secret}")
 
-small = True
-green = False
+# Chap4 연습문제 #2
+import random
+
+small = random.randint(0, 1)
+green = random.choice([True, False])
+
+print(small)
+print(green)
 
 if small:
     if green:
