c4edca9d5482f751c321594ae0be37a7b9fe5e06
Jeyoon <rj1031@naver.com>
2023-01-16 16:31:02 +0900

    Chap5 #4,5

---
+++ day03_assignment.py
@@ -0,0 +1,29 @@
+# Chap 5 #5.4, 5.5
+salutation = 'Mr.'
+name = 'Kim'
+product = 'other goods'
+verbed = 'sent'
+room = 'room'
+animals = 'dogs'
+amount = 'account'
+percent = '100'
+spokesman = 'James'
+job_title = 'costomer service representative'
+
+letter = '''Dear {} {},
+
+Thank you for your letter. We are sorry that our {} {} in your {}. Please note that it should never be used in a {}, especially near any {}.
+
+Send us your receipt and {} for shipping and handling. We will send you anouther {} that, in our tests, is {}% less likely to have {}.
+
+Thank you for your support.
+Sincerely,
+{}
+{}'''.format(salutation, name, product, verbed, room, room, animals, amount, product, percent, verbed, spokesman, job_title)
+
+print(letter)
+
+
+
+
+# Chap 5 #5.5
