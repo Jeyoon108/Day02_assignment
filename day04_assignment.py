ea1bf014d300f320bead6faac6d73e8a02cbd7cb
Jeyoon <rj1031@naver.com>
2023-01-17 18:48:26 +0900

    Chap7 #11

--- day04_assignment.py
+++ day04_assignment.py
@@ -1,5 +1,24 @@
-# Chap7 #10
+# Chap7 #11
 
-number_list = [number for number in range(10) if number % 2 == 0]
+start1 = ["fee", "fie", "foe"]
+rhymes = [
+    ("flop", "get a mop"),
+    ("fope", "turn the rope"),
+    ("fa", "get your ma"),
+    ("fudge", "call the judge"),
+    ("fat", "pet the cat"),
+    ("fog", "walk the dog"),
+    ("fun", "say we're done"),
+]
+start2 = "Someone better"
 
-print(number_list)
+# start 1 각 문자열 출력 후 rhymes의 첫 번째 출력 (첫 글자 대문자, 형태: 단어! 단어! 단어! 첫번째!)
+# start 2와 공백 출력 후 rhymes의 문자열과 마침표 출력
+
+first_start1 = []
+for song in rhymes:
+    for rap in start1:
+        print(rap.capitalize(), end='! ')
+    print(f'{song[0].capitalize()}!')
+    print(f'{start2} {song[1]}.')
+
