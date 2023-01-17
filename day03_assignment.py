5c3224978b9563a619b319f913557d58162c9fda
Jeyoon <rj1031@naver.com>
2023-01-17 09:16:42 +0900

    day03 last

--- day03_assignment.py
+++ day03_assignment.py
@@ -1,11 +1,2 @@
-# Chap 6 #3
-
-guess_me = int(input("Input any number : ")) #5
-
-for number in range(10):
-    if number < guess_me:
-        print(f"It's not {number}, still too low")
-    else:
-        print(f"found it! Input number was {guess_me}")
-        break
-
+word = "No" "way!"
+print(word)
