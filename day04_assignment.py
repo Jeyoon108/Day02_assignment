b7a58e86697e336b00f10fa9978377b2b21864c4
Jeyoon <rj1031@naver.com>
2023-01-17 17:07:30 +0900

    Chap7 #2,3

--- day04_assignment.py
+++ day04_assignment.py
@@ -1,8 +1,9 @@
-# Chap7 # 1
+# Chap7 #2 ~ #3
 
 i = int(input("출생년도를 입력하세요 : "))
 year_list = []
 for k in range(0, 5+1):
     year_list = year_list + [i+k]
 
-print(year_list)
+print(year_list[2])
+print(year_list[5])
